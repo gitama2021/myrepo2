f77f12f46e368b4d8571a2193cd7c02dce88d325
Alex2 <Alex2@LAPTOP-S07KN184>
2021-08-11 17:13:26 +0200

    2d commit

--- src/test1/test1_main.java
+++ src/test1/test1_main.java
@@ -4,7 +4,7 @@ public class test1_main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello test");
+System.out.println("Hello test2");
 	}
 
 }
